e6362ca0f34603b9840521ca13cfd3132671519d
Chris Mills <chris.mills@gorillalogic.com>
2019-11-18 16:47:43 -0800

    removed items

---
+++ eden.js
@@ -0,0 +1,52 @@
+{
+  "sections": [
+    {
+      "title": "Full Episodes",
+      "tiles": [
+        {
+          "title": "This Is Us",
+          "subtitle": "S1 E1 | 09/20/16",
+          "image_url": "https://stage-img.nbc.com/mpx-static/image/1018/743/160915_3100176_Series_Premiere_anvver_27.jpg?impolicy=nbc_com&imwidth=500",
+          "image_ratio": "16by9",
+          "is_playable": true,
+          "action_data": "/video/3100176"
+        },
+        {
+          "title": "Keeping up with the Kardashians",
+          "subtitle": "S17 E8 | 11/03/19",
+          "image_url": "https://stage-img.nbc.com/sites/nbcunbc/files/images/2019/11/03/20057d3c-0404-3082-934f-79e8b145db05.jpg?impolicy=nbc_com&imwidth=500",
+          "image_ratio": "16by9",
+          "is_playable": true,
+          "action_data": "/video/4053936"
+        },
+        {
+          "title": "Bluff City Law",
+          "subtitle": "S1 E1 | 09/23/19",
+          "image_url": "https://stage-img.nbc.com/mpx-static/image/209/803/190923_4034535_Pilot.jpg?impolicy=nbc_com&imwidth=500",
+          "image_ratio": "16by9",
+          "is_playable": true,
+          "action_data": "/video/4034535"
+        },
+        {
+          "title": "The Real Housewives of Orange County",
+          "subtitle": "S14 E100 | 08/01/19",
+          "image_url": "https://img.nbc.com/sites/nbcunbc/files/files/images/2019/8/01/d789905c-b941-3ca4-81f1-60c4d10a42cd.jpg?impolicy=nbc_com&imwidth=500",
+          "image_ratio": "16by9",
+          "is_playable": true,
+          "action_data": "/video/4055852"
+        }
+      ]
+    },
+    {
+      "title": "Featured Shows",
+      "tiles": [
+        {
+          "image_url": "https://img.nbc.com/sites/nbcunbc/files/files/images/2019/6/13/TemptationIsland-Season1-KeyArt-Lg-2048x1152.jpg",
+          "image_ratio": "16by9",
+          "is_playable": false,
+          "action_data": "/show/temptation-island"
+        }
+      ]
+    }
+  ]
+}
